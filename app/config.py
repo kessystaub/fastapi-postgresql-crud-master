e3727e65bb41ec3db2b79d1dc0c68b55e2f025ec
kessystaub <kessystaub@outlook.com>
2023-06-08 18:19:46 -0300

    supabase

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/python_db'
+# DATABASE_URL = 'postgresql://postgres:postgres@localhost:5433/python_db'
+
+DATABASE_URL = 'postgresql://postgres:saDASai128!@db.rlhstznswtigrzrdrpdv.supabase.co:5432/postgres'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
